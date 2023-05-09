27a8ddc2503879b2008a9bb17867031d592b3f89
yulgim <107096858+yulgim@users.noreply.github.com>
2023-05-09 23:05:28 +0900

    Update goto.js

--- goto.js
+++ goto.js
@@ -1,2 +1,9 @@
-var entity = document.querySelector('a-entity');
+//화살표마다 rotation변수 선언, 상황에 맞게 값 저장
+//각 화살표의 rotation = 위에서 만든 rotation 변수 적용
+//각 화살표의 id를 이용해 attribute 가능한지 확인하기
+const arw = 'arrow';
+for(int i=0; )
+const arwid[] = arw + ''
+
+var entity = document.querySelector('a-entity.');
 entity.setAttribute('position', '1 2 3');
