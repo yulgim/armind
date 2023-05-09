d31f2c6aaeaf23e5f46e8fe9872d233ae4145957
yulgim <107096858+yulgim@users.noreply.github.com>
2023-05-09 23:20:40 +0900

    Update goto.js

--- goto.js
+++ goto.js
@@ -1,9 +1,13 @@
 //화살표마다 rotation변수 선언, 상황에 맞게 값 저장
 //각 화살표의 rotation = 위에서 만든 rotation 변수 적용
 //각 화살표의 id를 이용해 attribute 가능한지 확인하기
+var imgmax = 5;
+var arwid = [];//화살표 이미지의 id
 const arw = 'arrow';
-for(int i=0; )
-const arwid[] = arw + ''
+for(int i = 0; i < imgmax; i++){
+  arwid.push(arw + string(i));
+}
+
 
 var entity = document.querySelector('a-entity.');
 entity.setAttribute('position', '1 2 3');
